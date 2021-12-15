36010036ab1dde461d1207e8fa8ff27a970e8980
Michael E <mceng.design@gmail.com>
2021-12-14 23:01:47 -0700

    removed old functions

--- day3/day3_part2.js
+++ day3/day3_part2.js
@@ -190,22 +190,6 @@ airSupply = (lifeSupportData) => {
   for (let i = 1; i < lsDataList.length; i++){
 
   }
-  // for( let i = 0; i < lifeSupportData[0].length; i++ ){
-  //   let count = 0;
-  //   for( let j = 0; j < lifeSupportData.length; j++ ){
-  //     if( lifeSupportData[j][i] === "1"){
-  //       count++;
-  //     }
-  //   }
-  //   if( count / lifeSupportData.length >= 0.5){
-  //     digit = "1";
-  //   } else if ( count / lifeSupportData.length < 0.5){
-  //     digit = "0";
-  //   }
-  //   if( lifeSupportData[j][i] === digit ){
-
-  //   }
-  // }
 }
 
 testData = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
